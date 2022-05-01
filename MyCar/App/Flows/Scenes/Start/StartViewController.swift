05de0bfde9e35cec6c6e04f758776d38986f2bd1
aksilont <aksilont@gmail.com>
2022-05-01 11:09:48 +0300

    Первоначальная настройка проекта

--- MyCar/App/Flows/Scenes/Start/StartViewController.swift
+++ MyCar/App/Flows/Scenes/Start/StartViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  StartViewController.swift
 //  MyCar
 //
 //  Created by Aksilont on 28.04.2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
