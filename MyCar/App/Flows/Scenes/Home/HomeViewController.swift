75e131724d47b376f82146a8ae7d4a794acb3a4c
aksilont <aksilont@gmail.com>
2022-05-11 14:20:27 +0300

    Добавлена кнопка Гараж на модуль Home

--- MyCar/App/Flows/Scenes/Home/HomeViewController.swift
+++ MyCar/App/Flows/Scenes/Home/HomeViewController.swift
@@ -12,10 +12,18 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = true
+    }
     
     @IBAction func exitDidTap(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func garageDidTap(_ sender: UIButton) {
+        let garageVC = GarageCollectionViewController()
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.pushViewController(garageVC, animated: true)
     }
 
 }
